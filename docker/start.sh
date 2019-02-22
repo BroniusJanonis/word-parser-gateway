df77c277bb62323398248b1b57612c84cc130cff
Bronius <Bronius.Janonis@inntec.lt>
2019-02-22 16:43:20 +0200

    Initial commit

---
+++ docker/start.sh
@@ -0,0 +1,1 @@
+java -Xmx256m -jar /opt/word-parser-gateway.jar
---
+++ src/main/java/lt/word/parser/gateway/server/GatewayApplication.java
@@ -0,0 +1,25 @@
+package lt.word.parser.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
+}
